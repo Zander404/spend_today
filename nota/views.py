5404604d3e5a934a6cb99b3dda6d7fd7431e0cdf
zander404 <alexandrearthurpg@gmail.com>
2024-08-04 20:28:06 -0300

    add

--- nota/views.py
+++ nota/views.py
@@ -4,6 +4,10 @@ from .models import Nota, Category
 
 # Create your views here.
 def index(request):
+    total: int = 0
     dados = Nota.objects.all()
 
-    return render(request, 'nota/index.html', {'dados': dados})
+    for item in dados:
+        total += item.value
+
+    return render(request, 'nota/index.html', {'dados': dados, 'total': total})
